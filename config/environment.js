ec6044bdc9c82d11d539ee08a3e58157e1adc759
felipekim9 <felipekim9@gmail.com>
2018-05-16 22:25:40 -0300

    Add pods structure.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'clickgram',
+    podModulePrefix: 'clickgram/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
