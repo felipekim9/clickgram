de10cf9f8f7927962d074b4ccb8c664aecb6856f
felipekim9 <felipekim9@gmail.com>
2018-06-14 23:04:55 -0300

    Create new attributes.

--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   createdAt: DS.attr('date'),
   updatedAt: DS.attr('date'),
-  image: DS.attr('string')
+  filename: DS.attr('string'),
+  filesize: DS.attr('string'),
+  url: DS.attr('string'),
+  user: DS.belongsTo('user')
 });
