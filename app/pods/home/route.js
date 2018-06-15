0af3e9aab5970692d159eaa8ab48d4c212e6c6be
felipekim9 <felipekim9@gmail.com>
2018-06-14 23:05:39 -0300

    Create a upload action.

--- app/pods/home/route.js
+++ app/pods/home/route.js
@@ -1,4 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    // return this.get('store').findRecord('user', 1)
+  },
+
+  actions: {
+    createPost() {
+      return this.get('store').createRecord('post');
+    }
+  }
+  // afterModel() {
+  //   this.get('store').createRecord('user', {
+  //     name: 'Felipe Kim',
+  //     username: 'felipekim9',
+  //     email: 'felipekim9@gmail.com',
+  //     gender: 'male',
+  //     image: ''
+  //   }).save();
+  // }
+
 });
